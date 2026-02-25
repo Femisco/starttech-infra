dc27a3184e58050e260df8046062ef62ee5063c8
Adeosun Ahmed <femisco29@gmail.com>
2026-02-25 19:15:47 +0100

    Fix terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "starttech-terraform-state-ahmed-2026"   # use your exact bucket name
+    bucket         = "starttech-terraform-state-ahmed-2026" # use your exact bucket name
     key            = "global/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
